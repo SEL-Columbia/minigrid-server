e7f70626e5e2555c317bd6bb46a0f62240b4798c
Viktor Roytman <vr2262@columbia.edu>
2017-04-27 14:52:38 +0300

    Further device simulation

--- dev/simulate_device.py
+++ dev/simulate_device.py
@@ -1,67 +1,78 @@
 #!/usr/bin/env python
 """Simulate requests from the device."""
 import argparse
-import http.client
 import os
 import re
 import sys
+from time import sleep
+from urllib.error import URLError
+from urllib.request import urlopen
+from uuid import UUID
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
 from sqlalchemy.orm import sessionmaker
 
+from commands import createdb
+
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 AES = algorithms.AES
 
+def uuid_string(minigrid_id):
+    return str(UUID(minigrid_id))
 
-#def createdb(ensure=True):
-#    """Create the schema and tables and return a Session."""
-#    from minigrid import models
-#    engine = models.create_engine()
-#    if ensure:
-#        models.Base.metadata.create_all(engine)
-#        print(f'Created schema {models.Base.metadata.schema}')
-#    return sessionmaker(bind=engine)()
-#
-#
-#def create_user(*, email):
-#    """Create a user with the given e-mail."""
-#    from minigrid import models
-#    session = createdb(ensure=False)
-#    with models.transaction(session) as tx_session:
-#        tx_session.add(models.User(email=email))
-#    print('Created user with e-mail ' + email)
-#
-#
-#def killdb():
-#    """Drop the schema."""
-#    from minigrid import models
-#    answer = input('You definitely want to kill the schema minigrid_dev? y/N ')
-#    if not answer.lower().startswith('y'):
-#        print('Not dropping the schema')
-#        return
-#    engine = models.create_engine()
-#    engine.execute('DROP SCHEMA minigrid_dev CASCADE')
-#    print('Dropped schema')
+
 def block_info(argument):
     if not re.match('^[a-fA-F\d]=.+$', argument):
         msg = (
             f"'{argument}' is not of the form block_number=value, where"
-            " block_number is 1 capital hex digit"
+            " block_number is 1 hex digit"
         )
         raise argparse.ArgumentTypeError(msg)
     return argument
-    
-def keep_posting(**blocks):
+
+
+def check_for_duplicates(blocks):
+    seen = set()
+    for raw_key, value in blocks:
+        key = int(raw_key, 16)
+        if key in seen:
+            raise argparse.ArgumentTypeError(f"duplicate block '{raw_key}'")
+        seen.add(key)
+
+
+def get_minigrid(minigrid_id):
+    from minigrid.models import get_minigrid
+    session = createdb(ensure=False)
+    return get_minigrid(session, minigrid_id)
+
+
+def keep_posting(endpoint, minigrid_id, **blocks):
     """POST the information every few seconds."""
-    assert False, blocks
+    msg = f'Connection refused at {endpoint}. Is the server running?'
+    minigrid = get_minigrid(minigrid_id)
+    assert False, minigrid.payment_system.aes_key
+    while True:
+        print('POSTing card info')
+        try:
+            response = urlopen(endpoint, data=b'deadbeef')
+        except URLError as error:
+            if isinstance(error.reason, ConnectionRefusedError):
+                print(msg)
+            else:
+                raise
+        else:
+            print(response.read())
+        sleep(2)
 
 
 def main():
     """Supply the information to POST."""
     parser = argparse.ArgumentParser()
+    parser.add_argument('minigrid_id', type=uuid_string)
+    parser.add_argument('--endpoint', default='http://localhost:8888')
     parser.add_argument('block', type=block_info, nargs='+')
     args, others = parser.parse_known_args()
     from minigrid.options import parse_command_line
@@ -69,7 +80,9 @@ def main():
     from minigrid.options import options
     if not any(other.startswith('--db_schema=') for other in others):
         options.db_schema = 'minigrid_dev'
-    keep_posting(**dict(kv.split('=', 1) for kv in args.block))
+    blocks = [kv.split('=', 1) for kv in args.block]
+    check_for_duplicates(blocks)
+    keep_posting(args.endpoint, args.minigrid_id, **dict(blocks))
 
 
 if __name__ == '__main__':
