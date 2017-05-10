5ab7f03c44066ceb3dad4b81d7e2bb3ff7078efe
Meredith Coulis <coulism@gmail.com>
2017-05-10 16:08:16 -0400

    webpack sockjs

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     app: path.resolve(__dirname, 'minigrid/static/src/js/minigrid_write_credit.js'),
   },
   output: {
-    path: path.resolve(__dirname, 'minigrid/static/bundles/'),
+    path: path.resolve(__dirname, 'minigrid/static/dist/'),
     filename: 'app.bundle.js',
   },
   module: {
