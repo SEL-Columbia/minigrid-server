835883452267334b5253a0c23c1ec301fa1f350a
Meredith Coulis <coulism@gmail.com>
2017-05-18 16:19:01 -0400

    tariff activation date and time that has already been set and template changes

--- minigrid/static/src/js/tariffs.js
+++ minigrid/static/src/js/tariffs.js
@@ -1,13 +1,21 @@
 var Flatpickr = require('flatpickr');
 require("flatpickr.css");
 
-
 var date_input = document.querySelector(".flatpickr-date");
 var time_input = document.querySelector(".flatpickr-time");
 
+if (timestamp) {
+	var datetime = timestamp.split('T')
+	var date = datetime[0];
+	var time = datetime[1];
+
+	date_input.value = date;
+	time_input.value = time;
+}
+
 const fp_date = new Flatpickr(date_input,
 	{
-		dateFormat: "d-m-Y",
+		dateFormat: "Y-m-d",
 		altInput: true,
 	})
 
@@ -16,5 +24,4 @@ const fp_time = new Flatpickr(time_input,
 		enableTime: true,
 		noCalendar: true,
 		time_24hr: true,
-		defaultDate: "00:00"
 	})
