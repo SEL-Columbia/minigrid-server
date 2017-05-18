bc2ab0817fb79b199b18804156356f55e9db6211
Meredith Coulis <coulism@gmail.com>
2017-05-18 12:12:32 -0400

    date-time picker

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,28 @@ module.exports = {
   entry: {
     write_credit: path.resolve(__dirname, 'minigrid/static/src/js/minigrid_write_credit.js'),
     read_card: path.resolve(__dirname, 'minigrid/static/src/js/read_card.js'),
+    tariffs: path.resolve(__dirname, 'minigrid/static/src/js/tariffs.js')
   },
   output: {
     path: path.resolve(__dirname, 'minigrid/static/dist/'),
     filename: '[name].bundle.js',
   },
   module: {
-    rules: [
+    loaders: [
       {
-        test: /\.js$/,
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      { test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        }
+        loader: 'babel-loader'
       }
     ]
+  },
+  resolve: {
+    modules: ['node_modules', 'src'],
+      alias: {
+        'flatpickr.css': path.join(__dirname, 'node_modules/flatpickr/dist/flatpickr.css')
+      }
   }
 }
