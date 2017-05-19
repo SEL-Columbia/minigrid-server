9b95aee0fb060f0a8682c2d0ea095d9187042d78
Meredith Coulis <coulism@gmail.com>
2017-05-19 11:19:57 -0400

    single tariff input

--- minigrid/static/src/js/tariffs.js
+++ minigrid/static/src/js/tariffs.js
@@ -1,27 +1,12 @@
 var Flatpickr = require('flatpickr');
 require("flatpickr.css");
 
-var date_input = document.querySelector(".flatpickr-date");
-var time_input = document.querySelector(".flatpickr-time");
+var date_input = document.querySelector(".flatpickr");
 
-if (timestamp) {
-	var datetime = timestamp.split('T')
-	var date = datetime[0];
-	var time = datetime[1];
-
-	date_input.value = date;
-	time_input.value = time;
-}
+if (timestamp) date_input.value = timestamp;
 
 const fp_date = new Flatpickr(date_input,
 	{
-		dateFormat: "Y-m-d",
-		altInput: true,
-	})
-
-const fp_time = new Flatpickr(time_input,
-	{
 		enableTime: true,
-		noCalendar: true,
-		time_24hr: true,
+		time_24hr: true
 	})
