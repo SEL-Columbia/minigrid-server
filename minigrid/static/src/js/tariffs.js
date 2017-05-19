e9e02c72a2c02970265880bbf367243b332f20bc
Meredith Coulis <coulism@gmail.com>
2017-05-19 11:49:34 -0400

    default date now for initializing info

--- minigrid/static/src/js/tariffs.js
+++ minigrid/static/src/js/tariffs.js
@@ -4,9 +4,11 @@ require("flatpickr.css");
 var date_input = document.querySelector(".flatpickr");
 
 if (timestamp) date_input.value = timestamp;
+else date_input.value = new Date(Date.now()).toISOString();
 
 const fp_date = new Flatpickr(date_input,
 	{
 		enableTime: true,
-		time_24hr: true
+		time_24hr: true,
+		minuteIncrement: 1
 	})
