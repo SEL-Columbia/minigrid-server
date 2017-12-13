935af5bbad4ddebf79dfc1034470468d1919fb39
jmbott <millerbott@gmail.com>
2017-12-13 03:19:10 -0500

    update models

--- minigrid/models.py
+++ minigrid/models.py
@@ -326,7 +326,9 @@ class Customer(Base):
 
     __table_args__ = (
         sa.UniqueConstraint('customer_minigrid_id', 'customer_user_id'),
-        sa.UniqueConstraint('customer_minigrid_id', 'customer_name'))
+        sa.UniqueConstraint('customer_minigrid_id', 'customer_name')),
+        sa.UniqueConstraint('customer_minigrid_id', 'customer_current_limit')),
+        sa.UniqueConstraint('customer_minigrid_id', 'customer_energy_limit'))
 
 
 class MaintenanceCard(Base):
