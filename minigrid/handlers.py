03261683d8dd605fda2bb87a1067d8077528f2fa
jmbott <millerbott@gmail.com>
2018-07-31 16:04:50 -0400

    execute_command not config_set
    Signed-off-by: jmbott <millerbott@gmail.com>

--- minigrid/handlers.py
+++ minigrid/handlers.py
@@ -37,7 +37,8 @@ AES = algorithms.AES
 cache = redis.StrictRedis.from_url(options.redis_url)
 broker_url = 'https://broker.portier.io'
 
-cache.config_set('notify-keyspace-events', 'Ex')
+# cache.config_set('notify-keyspace-events', 'Ex') # not available in fakeRedis
+cache.execute_command('CONFIG SET', 'notify-keyspace-events', 'Ex')
 pubsub = cache.pubsub()
 pubsub.subscribe("__keyevent@0__:expired")
 
